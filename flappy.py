6c015cefa39bd7ddca04043cc71f7c3acb572816
andrei.int <andrei_ioan.ionita@stud.acs.upb.ro>
2021-05-09 18:57:46 +0300

    Added comentaries and made few movement system changes

--- flappy.py
+++ flappy.py
@@ -1,18 +1,20 @@
-
-
 import pygame, sys, random
 
 pygame.init()
+
 pygame.display.set_caption('Flappy Bird')
 clock = pygame.time.Clock()
-window=pygame.display.set_mode((500,700))   #dimensiunile alese astfel incat imaginea
+window = pygame.display.set_mode((500,700))   #dimensiunile alese astfel incat imaginea
                                              #de fundal sa fie jumatate din acestea  
-img_surface=pygame.image.load('img\mobile.jpg')  
+img_surface = pygame.image.load('img/mobile.jpg')  
 
-ship_surface = pygame.image.load('img\spaceship.png') 
+ship_surface = pygame.image.load('img/spaceship.png') 
 ship_rect = ship_surface.get_rect(center = (150, 450))
 
+# Different images for the asteroids
+
 asteroid_images = []
+asteroid_list = []
 
 ast_surface1 = pygame.transform.scale(pygame.image.load("asteroid1.png"), (100, 100))
 ast_surface2 = pygame.transform.scale(pygame.image.load("asteroid2.png"), (100, 100))
@@ -23,7 +25,6 @@ asteroid_images.append(ast_surface2)
 asteroid_images.append(ast_surface3)
 
 def create_asteroid(positionY):
-    # random_asteroid_position = random.randint(100, 600)
     selected_asteroid = random.choice(asteroid_images)
     asteroid = selected_asteroid.get_rect(midtop = (800, positionY))
     return (asteroid, selected_asteroid)
@@ -37,18 +38,21 @@ def draw_asteroid(asteroids):
     for asteroid_tuple in asteroids:
         window.blit(asteroid_tuple[1], asteroid_tuple[0])
 
-# def remove_asteroid(asteroids):
+# Collision system
+
+# def check_for_crash(asteroids):
 #     for asteroid_tuple in asteroids:
-#         if asteroid_tuple[0].centerx < -100:
-#             asteroids.remove(asteroid_tuple)
+#         if ship_rect.colliderect(asteroid_tuple[0]):
+#             print('collision')
+
+# Movement on the y axis for the spaceshit
 
 ship_position = 0
 gravity = 0.05
 
-asteroid_list = []
 SPAWNOBSTACLE = pygame.USEREVENT
 
-pygame.time.set_timer(SPAWNOBSTACLE, 1000)
+pygame.time.set_timer(SPAWNOBSTACLE, 1100)
 
 def get_rand_position():
     pos1 = random.randint(100, 600)
@@ -64,12 +68,16 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 ship_position = 0
                 ship_position -= 3
+
+        # Spawning asteroird every 1.1 seconds
+        
         if event.type == SPAWNOBSTACLE:
-            number_of_asteroids = random.randint(1, 2)
+            number_of_asteroids = random.randint(1, 3)
             if number_of_asteroids == 1:
                 asteroid_list.append(create_asteroid(random.randint(100, 600)))
             else:
@@ -78,21 +86,30 @@ while True:
                 asteroid_list.append(create_asteroid(position[0]))
                 asteroid_list.append(create_asteroid(position[1]))
 
+
+    # Loading background
+
     window.blit(pygame.transform.scale(img_surface, (500, 700)), (0, 0))
 
-    ship_position += gravity
-
-    ship_rect.centery += ship_position
-
-
-    asteroid_list = move_asteroid(asteroid_list)
-    # asteroid_list = remove_asteroid(asteroid_list)
-    draw_asteroid(asteroid_list)
+    # Drawing the spaceship
 
     window.blit(pygame.transform.scale(ship_surface, (80, 60)), ship_rect)
+    # check_for_crash(asteroid_list)
+
+    # Spaceship movement on Y axis
+
+    ship_position += gravity
+    ship_rect.centery += ship_position
+
+    # Generating and drawing asteroids
+
+    asteroid_list = move_asteroid(asteroid_list)
+    
+    draw_asteroid(asteroid_list)
+
     pygame.display.update()
 
-    clock.tick(100)  #trebuie sa fie >30
+    clock.tick(90)  
 
 
 #pentru a pune pipe-uri se creeaza o lista de rectangles 
