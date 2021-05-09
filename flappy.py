b07f4953c76f177f57c951f03249046fd29a6113
Your Name <you@example.com>
2021-05-09 12:45:38 +0300

    img

--- flappy.py
+++ flappy.py
@@ -1,17 +1,22 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 
 import pygame, sys, random
 
 pygame.init()
+pygame.display.set_caption('Flappy Bird')
 clock = pygame.time.Clock()
-window=pygame.display.set_mode((700,1000))   #dimensiunile alese astfel incat imaginea
-                                             #de fundal sa fie jumatate din acestea         
+window=pygame.display.set_mode((500,700))   #dimensiunile alese astfel incat imaginea
+                                             #de fundal sa fie jumatate din acestea  
+img_surface=pygame.image.load('img/mobile.jpg')   
+ship_surface=pygame.image.load('img/space.png') 
+
 while True:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
             pygame.quit()
             sys.exit()
+    window.blit(pygame.transform.scale(img_surface, (500, 700)), (0, 0))
+    window.blit(pygame.transform.scale(ship_surface, (100, 100)), (0, 0))
     pygame.display.update()
 
     clock.tick(100)  #trebuie sa fie >30
@@ -23,7 +28,4 @@ while True:
 # pt a da flip la surface : pygame.transform.flip(surface,False,True) - flip pe y
 # pt rotatie : pygame.transform.rotozoom(surface, unghiul cu care vrem sa facem rotatia,1)
 
-=======
-print(Testing commit)
-print("uwduiw")
->>>>>>> ae8b9472dd6d27e33efc74b4d3fe2232b9bdf57f
+
